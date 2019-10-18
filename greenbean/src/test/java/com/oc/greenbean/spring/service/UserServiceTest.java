ab2f23af5920916d615eed436a95ad8b34ee4432
Oliver <oliverstill1@gmail.com>
2019-10-18 14:35:29 +0800

    Go

---
+++ greenbean/src/test/java/com/oc/greenbean/spring/service/UserServiceTest.java
@@ -0,0 +1,23 @@
+package com.oc.greenbean.spring.service;
+
+import org.junit.jupiter.api.Test;
+
+public class UserServiceTest {
+
+    @Test
+    public void testInsertUser() {
+        //1.环境搭建
+        UserService userService = new UserService();
+        //还需要 UserMapper
+
+        // 2.传入参数
+        // 需要 User
+
+        //3.调用
+
+        //4.断言结果
+
+
+
+    }
+}
