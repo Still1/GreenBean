9ec43e6275836b9da258695247ac96302b55e1fc
Oliver <oliverstill1@gmail.com>
2019-10-27 22:13:23 +0800

    7.37 重构优化UserServiceTest

--- greenbean/src/test/java/com/oc/greenbean/spring/service/UserServiceTest.java
+++ greenbean/src/test/java/com/oc/greenbean/spring/service/UserServiceTest.java
@@ -9,12 +9,15 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InOrder;
 import org.mockito.Mockito;
 
-import java.util.List;
+import java.util.*;
 
 class UserServiceTest {
 
     private static String existUsername = "exist";
     private static String notExistUsername = "notExist";
+    private static String defaultPassword = "password";
+    private static Integer existId = 100;
+    private static Integer userIdGeneratedByDatabase = 555;
 
     private UserService userService;
     private UserMapper mockUserMapper;
@@ -28,43 +31,56 @@ class UserServiceTest {
     @SuppressWarnings("unchecked")
     @Test
     void testInsertUser() {
-        int mockUserId = 555;
         Mockito.when(mockUserMapper.getUserByUsername(notExistUsername)).thenReturn(null);
+        User user = this.generateUser(false);
         Mockito.doAnswer((invocationOnMock) -> {
-            User mockUser = invocationOnMock.getArgument(0);
-            Mockito.when(mockUser.getId()).thenReturn(mockUserId);
+            User userArgument = invocationOnMock.getArgument(0);
+            userArgument.setId(userIdGeneratedByDatabase);
             return null;
-        }).when(mockUserMapper).insertUserBasicInfo(Mockito.any(User.class));
-        User mockUser = Mockito.mock(User.class);
-        Mockito.when(mockUser.getUsername()).thenReturn(notExistUsername);
-        List mockAuthority = Mockito.mock(List.class);
-        Mockito.when(mockAuthority.get(0)).thenReturn("USER");
-        Mockito.when(mockUser.getAuthority()).thenReturn(mockAuthority);
-        userService.insertUser(mockUser);
+        }).when(mockUserMapper).insertUserBasicInfo(user);
+        userService.insertUser(user);
         InOrder inOrder = Mockito.inOrder(mockUserMapper);
-        inOrder.verify(mockUserMapper).insertUserBasicInfo(mockUser);
-        inOrder.verify(mockUserMapper).insertUserAuthority(mockUserId, mockAuthority);
+        inOrder.verify(mockUserMapper).insertUserBasicInfo(user);
+        inOrder.verify(mockUserMapper).insertUserAuthority(userIdGeneratedByDatabase, user.getAuthority());
     }
 
     @Test
     void testInsertUserWithDuplicatedUsername() {
-        Mockito.when(mockUserMapper.getUserByUsername(existUsername)).thenReturn(Mockito.mock(User.class));
-        User mockUser = Mockito.mock(User.class);
-        Mockito.when(mockUser.getUsername()).thenReturn(existUsername);
-        Assertions.assertThrows(UsernameDuplicatedException.class, () -> userService.insertUser(mockUser));
+        User existUser = this.generateUser(true);
+        Mockito.when(mockUserMapper.getUserByUsername(existUsername)).thenReturn(existUser);
+        User user = new User();
+        user.setUsername(existUsername);
+        Assertions.assertThrows(UsernameDuplicatedException.class, () -> userService.insertUser(user));
     }
 
     @Test
     void testValidateUsernameDuplicatedExist() {
-        Mockito.when(mockUserMapper.getUserByUsername(existUsername)).thenReturn(Mockito.mock(User.class));
-        boolean validateUserA = userService.validateUsernameDuplicated(existUsername);
-        Assertions.assertFalse(validateUserA);
+        User existUser = this.generateUser(true);
+        Mockito.when(mockUserMapper.getUserByUsername(existUsername)).thenReturn(existUser);
+        boolean validateUser = userService.validateUsernameDuplicated(existUsername);
+        Assertions.assertFalse(validateUser);
     }
 
     @Test
     void testValidateUsernameDuplicatedNotExist() {
         Mockito.when(mockUserMapper.getUserByUsername(notExistUsername)).thenReturn(null);
-        boolean validateUserB = userService.validateUsernameDuplicated(notExistUsername);
-        Assertions.assertTrue(validateUserB);
+        boolean validateUser = userService.validateUsernameDuplicated(notExistUsername);
+        Assertions.assertTrue(validateUser);
+    }
+
+    private User generateUser(boolean exist) {
+        User user = new User();
+        if(exist) {
+            user.setId(existId);
+            user.setUsername(existUsername);
+        } else {
+            user.setUsername(notExistUsername);
+        }
+        user.setPassword(defaultPassword);
+        user.setEnabled(true);
+        List<String> authority = new ArrayList<>();
+        authority.add("USER");
+        user.setAuthority(authority);
+        return user;
     }
 }
