d09d1982bdca598a2e3d70c74686bcc1de33dbe2
Oliver <oliverstill1@gmail.com>
2019-10-28 22:07:00 +0800

    7.47 MyBatis XML配置

--- greenbean/src/main/java/com/oc/greenbean/mybatis/mapper/UserMapper.java
+++ greenbean/src/main/java/com/oc/greenbean/mybatis/mapper/UserMapper.java
@@ -1,10 +1,7 @@
 package com.oc.greenbean.mybatis.mapper;
 
 import com.oc.greenbean.domain.User;
-import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.SelectKey;
 
 import java.util.List;
 
@@ -13,23 +10,12 @@ public interface UserMapper {
 
     //XXX 把SQL语句写回到XML配置文件中
 
-    @Select("select * from t_user where id = #{id}")
     @SuppressWarnings("unused")
     User getUserById(Integer id);
 
-    @Select("select * from t_user where username = #{username}")
     User getUserByUsername(String username);
 
-    @Insert({"insert into t_user (username, password, enabled) ",
-        "values(#{username}, #{password}, #{enabled})"})
-    @SelectKey(statement="select last_insert_id()", keyProperty="id", before=false, resultType=Integer.class)
     void insertUserBasicInfo(User user);
 
-    @Insert({"<script>",
-        "insert into t_authority (user_id, authority) values ",
-        "<foreach item='item' collection='authority' separator=','>",
-        "(#{userId}, #{item})",
-        "</foreach>",
-        "</script>"})
     void insertUserAuthority(@Param("userId") Integer userId, @Param("authority") List<String> authority);
 }
