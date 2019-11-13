bed1f485c06ca80a2f4c956515c2e38a0a10a6be
Oliver <oliverstill1@gmail.com>
2019-11-13 21:51:42 +0800

    MyBatis cache done.

--- greenbean/src/main/java/com/oc/greenbean/domain/User.java
+++ greenbean/src/main/java/com/oc/greenbean/domain/User.java
@@ -1,9 +1,10 @@
 package com.oc.greenbean.domain;
 
+import java.io.Serializable;
 import java.util.List;
 import java.util.Objects;
 
-public class User {
+public class User implements Serializable {
     private Integer id;
     private String username;
     private String password;
