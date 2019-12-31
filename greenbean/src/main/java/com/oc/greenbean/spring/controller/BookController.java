d8e751f4d29a27f1086b7b664a72fc9f1eb5dded
Oliver <oliverstill1@gmail.com>
2019-12-31 12:27:34 +0800

    11.2.2 页面图书信息区以及评分区初步

--- greenbean/src/main/java/com/oc/greenbean/spring/controller/BookController.java
+++ greenbean/src/main/java/com/oc/greenbean/spring/controller/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.thymeleaf.util.StringUtils;
@@ -77,6 +78,11 @@ public class BookController {
     @ResponseBody
     public List<String> getTranslatorSuggestion(String keyword) {
         return this.bookService.getTranslatorSuggestion(keyword);
+    }
+
+    @GetMapping("/book/{id}")
+    public String showBook(@PathVariable Integer id) {
+        return "book";
     }
 
     //TODO 出错视图
