5bf476e813e2fc191ff94ce904c6392f746f528c
Oliver Chung <oliverstill1@gmail.com>
2023-02-22 13:24:32 +0800

    解决tomcat处理post请求参数中文乱码问题

--- greenbean/src/main/java/com/oc/greenbean/web/setting/SecurityWebApplicationInitializer.java
+++ greenbean/src/main/java/com/oc/greenbean/web/setting/SecurityWebApplicationInitializer.java
@@ -1,6 +1,14 @@
 package com.oc.greenbean.web.setting;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.servlet.ServletContext;
+import java.nio.charset.StandardCharsets;
 
 public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+    @Override
+    protected void beforeSpringSecurityFilterChain(ServletContext servletContext) {
+        insertFilters(servletContext, new CharacterEncodingFilter(StandardCharsets.UTF_8.name()));
+    }
 }
